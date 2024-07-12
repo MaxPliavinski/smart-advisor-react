8b1f1296d04a852386df724a9ed1a2fed00117fe
MaxPliavinski <maksim.pliavinski.dev@gmail.com>
2024-07-12 22:55:16 +0300

    Add "fake auth"

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,19 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { RootLayout } from './layouts/RootLayout';
 import { HomePage } from './pages/HomePage';
 import './styles/index.css';
+import { LoginPage } from './pages/LoginPage';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <RootLayout />,
-    children: [{ path: '', element: <HomePage /> }],
+    children: [
+      { path: '', element: <HomePage /> },
+      {
+        path: 'login',
+        element: <LoginPage />,
+      },
+    ],
   },
 ]);
 
--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,9 +1,27 @@
 import { Button } from 'flowbite-react';
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export const LoginPage = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const isLoggedIn = localStorage.getItem('isLoggedIn');
+
+    if (isLoggedIn) {
+      return navigate('/');
+    }
+  }, []);
+
+  const handleLogin = () => {
+    localStorage.setItem('isLoggedIn', 'true');
+
+    navigate('/');
+  };
+
   return (
     <div className='flex w-full items-center justify-center'>
-      <Button>Login</Button>
+      <Button onClick={handleLogin}>Login</Button>
     </div>
   );
 };
