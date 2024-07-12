7797b132ab326748a5a3a72ee544f2655f3c5222
MaxPliavinski <maksim.pliavinski.dev@gmail.com>
2024-07-12 22:55:16 +0300

    Add "fake auth"

--- src/components/shared/Header/SettingsInfo/index.jsx
+++ src/components/shared/Header/SettingsInfo/index.jsx
@@ -1,16 +1,20 @@
 import { SAFlag } from '@/components/icons';
 import { Button } from 'flowbite-react';
+import { useNavigate } from 'react-router-dom';
 
 export const SettingsInfo = () => {
+  const navigate = useNavigate();
+
   const handleLogout = () => {
     localStorage.removeItem('isLoggedIn');
+    navigate('/login');
   };
 
   return (
     <div className='flex items-center gap-8'>
       <div className='flex items-center gap-2'>
         <SAFlag />
-        <span className='text-gray900 text-sm font-medium'>العربية</span>
+        <span className='text-sm font-medium text-gray900'>العربية</span>
       </div>
       <Button
         size='sm'
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,15 +1,29 @@
+import { useNavigate } from 'react-router-dom';
 import { BusinessLocationForm } from '../components/forms/BusinessLocationForm';
 import InteractiveMap from '../components/Map';
 import { Header } from '../components/shared';
+import { useEffect } from 'react';
 
-export const HomePage = () => (
-  <div className='flex min-h-full w-full flex-col bg-[#FCFAFA]'>
-    <Header />
-    <div className='flex'>
-      <div className='py-[32px] pl-[56px] pr-[80px]'>
-        <BusinessLocationForm className='w-full max-w-[550px]' />
+export const HomePage = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const isLoggedIn = localStorage.getItem('isLoggedIn');
+
+    if (!isLoggedIn) {
+      return navigate('/login');
+    }
+  }, []);
+
+  return (
+    <div className='flex min-h-full w-full flex-col bg-[#FCFAFA]'>
+      <Header />
+      <div className='flex'>
+        <div className='py-[32px] pl-[56px] pr-[80px]'>
+          <BusinessLocationForm className='w-full max-w-[550px]' />
+        </div>
+        <InteractiveMap />
       </div>
-      <InteractiveMap />
     </div>
-  </div>
-);
+  );
+};
