3e7dc97574b6c2443143ecc655daa380399382af
MaxPliavinski <maksim.pliavinski.dev@gmail.com>
2024-07-12 20:43:05 +0300

    Update active step search param after submit

--- src/components/shared/Header/index.jsx
+++ src/components/shared/Header/index.jsx
@@ -1,11 +1,15 @@
 import { Stepper } from '@/components/ui';
 import { FORM_STEPS } from '@/data';
 import { SettingsInfo } from './SettingsInfo';
+import { useSearchParams } from 'react-router-dom';
 
 export const Header = () => {
+  const [searchParams] = useSearchParams();
+  const activeStep = searchParams.get('step');
+
   return (
     <header className='flex w-full justify-between py-6 pl-[52.5px] pr-6'>
-      <Stepper steps={FORM_STEPS} />
+      <Stepper steps={FORM_STEPS} activeStep={activeStep} />
       <SettingsInfo />
     </header>
   );
