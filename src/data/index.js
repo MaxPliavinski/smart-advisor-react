6f2c3635d3475e3269995483743af3b277d004c8
MaxPliavinski <maksim.pliavinski.dev@gmail.com>
2024-07-12 17:32:51 +0300

    Create "Input" component

--- src/data/index.js
+++ src/data/index.js
@@ -1,1 +1,32 @@
 export const FORM_STEPS = ['basics', 'activities', 'premise', 'results'];
+
+export const businessLocationFormFields = [
+  {
+    id: 'amana',
+    type: 'select',
+    label: 'Amana',
+    options: ['United States', 'Canada'],
+    required: true,
+  },
+  {
+    id: 'municipality',
+    type: 'select',
+    label: 'Municipality',
+    options: ['United States', 'Canada'],
+    required: true,
+  },
+  {
+    id: 'district',
+    type: 'input',
+    label: 'District',
+    placeholder: 'Enter a district',
+    required: false,
+  },
+  {
+    id: 'street',
+    type: 'input',
+    label: 'Street',
+    placeholder: 'Enter a street',
+    required: false,
+  },
+];
