5ac37bad0e579ae57bc49cbe1fc425ed78c622ef
MaxPliavinski <maksim.pliavinski.dev@gmail.com>
2024-07-12 20:35:13 +0300

    Add basic form validation

--- src/components/common/Input/index.jsx
+++ src/components/common/Input/index.jsx
@@ -1,7 +1,19 @@
-export const Input = ({ className, ...props }) => (
+import { forwardRef } from 'react';
+import cn from 'classnames';
+
+export const InputWithoutRef = ({ className, error, ...props }, ref) => (
   <input
     type='text'
-    className={`border-gray200 text-gray900 block w-full rounded-lg border bg-white p-3 text-sm leading-tight focus:border-cyan-500 focus:ring-cyan-500 ${className}`}
+    className={cn(
+      `block w-full rounded-lg border border-gray200 bg-white p-3 text-sm leading-tight text-gray900 focus:border-cyan-500 focus:ring-cyan-500 ${className}`,
+      {
+        'border-red-600 bg-red-500/15 text-red-500 focus:border-red-500 focus:ring-red-500':
+          error,
+      },
+    )}
+    ref={ref}
     {...props}
   />
 );
+
+export const Input = forwardRef(InputWithoutRef);
--- src/components/common/Select/index.jsx
+++ src/components/common/Select/index.jsx
@@ -1,8 +1,13 @@
-export const Select = ({ className = '', children, ...props }) => (
+import { forwardRef } from 'react';
+
+const SelectWithoutRef = ({ className = '', children, ...props }, ref) => (
   <select
-    className={`border-gray200 block w-full appearance-none rounded-lg border bg-white !bg-[url('@/assets/arrow-down.svg')] p-3 text-sm leading-tight text-gray-900 focus:border-cyan-500 focus:ring-cyan-500 ${className}`}
+    className={`block w-full appearance-none rounded-lg border border-gray-200 bg-white bg-[url('/assets/arrow-down.svg')] p-3 text-sm leading-tight text-gray-900 focus:border-cyan-500 focus:ring-cyan-500 ${className}`}
+    ref={ref}
     {...props}
   >
     {children}
   </select>
 );
+
+export const Select = forwardRef(SelectWithoutRef);
